d2f1a5ecc95539a75daba4898dd73916bd5d7cef
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-25 23:38:23 +0100

    Update Slide_Streamlit.py

--- Berenger/Slide_Streamlit.py
+++ Berenger/Slide_Streamlit.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 st.set_page_config(page_title='Music Popularity Analysis', page_icon=':musical_note:')
-df = pd.read_csv('..\data\dataset_algo.csv.zip', index_col=0)
+df = pd.read_csv('../data/dataset_algo.csv.zip', index_col=0)
 
 
 def _max_width_():
@@ -26,11 +26,11 @@ _max_width_()
 
 @st.cache
 def load_data_polar():
-    return pd.read_csv('data/top_polar.csv.zip')
+    return pd.read_csv('..data/top_polar.csv.zip')
 
 @st.cache
 def load_data_ml():
-    return pd.read_csv('data/music_ml.csv.zip')
+    return pd.read_csv('..data/music_ml.csv.zip')
 
 data_polar_top = load_data_polar()
 data_ml = load_data_ml()
@@ -41,78 +41,9 @@ st.title('Music Track Analysis Project')
 def main():
 
     pages = {
-        'Home': home,
-        'Scenario': scenario,
-        'Best Parameters': music_details,
         'Popularity Estimator': popularity_estimator
         }
 
-    if "page" not in st.session_state:
-        st.session_state.update({
-        # Default page
-        'page': 'Home'
-        })
-
-    with st.sidebar:
-        st.image('assets/logo_data_yoyo.png', width=200)
-        page = st.selectbox("Choose a page", tuple(pages.keys()))
-
-    pages[page]()
-
-
-def home():
-
-    st.image('assets/logo_data_yoyo.png', width=400)
-    st.subheader('About this project')
-
-    'This project was completed during a 33 hours hackathon, and the subject is music.'
-    'We tried to determine what makes a music track popular, and to estimate the popularity of a track by tweaking its characteristics.'
-    'This app is brought to you by the awesome Data Yoyo Team:'
-    ''
-    col1, col2, col3 = st.columns(3)
-    
-    with col1:
-        st.markdown('[Guillaume Arp](https://github.com/GuillaumeArp)')
-        st.image('assets/guillaume.png')
-        
-    with col2:
-        st.markdown('[Nizar Ben Slama](https://github.com/bennizar87)')
-        #st.image('')
-        
-    with col3:
-        st.markdown('[Alexandra Houssin](hhttps://github.com/alexandrahoussin)')
-        #st.image('')    
-    
-    ''    
-    col4, col5, col6 = st.columns(3)
-    
-    with col4:
-        st.markdown('[Catherine Le Calve](https://github.com/CathieLC)')
-        st.image('assets/cath.png')
-        
-    with col6:
-        st.markdown('[Bérenger Queune](https://github.com/BerengerQueune)')
-        #st.image('')
-        
-def scenario():
-    
-    st.subheader('Scenario')
-    
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.'
-
-def music_details():
-    
-    st.subheader('What makes a track popular?')
-    
-    'Polar chart showing top music characteristics.'
-    
-    carac = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness',  'valence', 'loudness_scaled']
-    fig = px.line_polar(data_polar_top, theta=carac, r= data_polar_top[carac].mean(), line_close=True, template="plotly_dark", 
-                        color_discrete_sequence=px.colors.sequential.Plasma_r)
-    fig.update_traces(fill='toself')
-    
-    fig.update_layout(height=900)
-    st.plotly_chart(fig, use_container_width=True)
 
 def popularity_estimator():
     
