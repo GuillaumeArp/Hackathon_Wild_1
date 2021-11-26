a02f141380de409d15ef6cdf5ac53c58b03a07f1
Guillaume Arp <guillaume.arp@gmail.com>
2021-11-26 01:13:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -330,7 +330,7 @@ def popularity_estimator():
         st.write(" ")
         
     with col3:
-        if st.button('Click Here to run the algorithm'):
+        if st.button('Click Here'):
             #########
             resultat = algo_popularity()
             if resultat == 1:
